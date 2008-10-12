c2b1618cf8108b0e17cc310d01c511a3ee631cf3
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2008-10-12 14:55:56 -0400

    skip games already played; set correct year for game dates; change format of event summary to be 'home team vs. away team'

--- overhead_door_schedule.rb
+++ overhead_door_schedule.rb
@@ -10,11 +10,21 @@ games = schedule.search('table#ctl00_C_Schedule1_GridView1 > tr.RowStyle | table
 games.each do |game|
   raw_details = game.children_of_type('td')
   details = raw_details.map { |d| d.inner_text.to_s.strip }
-  # details.each_with_index { |d, i| puts "[#{i}] = #{d}" }
+  
+  next if details[4] == "Final" # skip games already played
+  
+  game_date_regex = /\w{3}-(\w{3}) (\d{1,2})/
+  match_data = game_date_regex.match(details[0])
+  
+  raise ArgumentError, "invalid game date: #{details[0]}" if match_data.nil?
+  
+  month, day = match_data[1], match_data[2]
+  year = %w(Jan Feb Mar).include?(month) ? 2009 : 2008
+  
   event = Icalendar::Event.new
-  event.summary = "#{details[3]} @ #{details[1]}"
-  event.location = "Twin Star: #{details[5]}"
-  event.dtstart = DateTime.parse("#{details[0]} at #{details[4]}")
+  event.summary = "#{details[1]} vs. #{details[3]}"
+  event.location = "S2: #{details[5]}"
+  event.dtstart = DateTime.parse("#{month} #{day}, #{year} #{details[4]}")
   event.duration = "PT1H" # 1 hour
     
   calendar.add_event(event)
